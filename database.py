6157db0a8973492b93d475bb944c9e59e60f34c4
31207 <1692038362@qq.com>
2025-03-16 23:24:05 +0800

    config identified

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import config
 # 替换成你的MySQL连接信息
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mm123123@localhost/chat_db"
+SQLALCHEMY_DATABASE_URL = config.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
