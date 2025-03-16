95ea0bd0f13919cdfd1ba1e5316e65ea54c548e2
31207 <1692038362@qq.com>
2025-03-17 00:54:26 +0800

    absolute path

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import yaml
-with open("config.yaml", "r", encoding="utf-8") as file:
+with open("/home/ecs-user/chatroom_server/config.yaml", "r", encoding="utf-8") as file:
     data = yaml.safe_load(file)
 
 SECRET_KEY = data["SECRET_KEY"]
